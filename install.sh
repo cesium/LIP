d85aede0bef20a1d7d8dbd76738e209b00f4dd6e
Nelson Estevão <nelsonmestevao@gmail.com>
2018-09-24 18:00:32 +0100

    Fix errors on install
    - add curl
    - remove yes thing
    - fix svn (subversion)

--- install.sh
+++ install.sh
@@ -5,13 +5,16 @@ sudo apt update
 sudo apt upgrade
 
 # install haskell
-sudo apt install haskell-platform
+sudo apt install haskell-platform -yqq
 
 # install chromium browser
-sudo apt install chromium-browser
+sudo apt install chromium-browser -yqq
 
 # install verion controll (svn and git)
-sudo apt install svn git
+sudo apt install subversion git -yqq
+
+# install curl
+sudo apt install curl -yqq
 
 # install vscode
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
@@ -20,7 +23,7 @@ sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode s
 
 sudo apt install apt-transport-https
 sudo apt update
-sudo apt install code # or code-insiders
+sudo apt install code -yqq# or code-insiders
 
 # set vscode as the default editor (https://wiki.debian.org/DebianAlternative)
 sudo update-alternatives --set editor /usr/bin/code
