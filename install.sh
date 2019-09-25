1826b43012657d798912e24f7aa0bb50a204755c
MightyMime <jose.filipe.matos.ferreira@gmail.com>
2019-09-25 14:34:40 +0100

    add manjaro suport

--- install.sh
+++ install.sh
@@ -1,29 +1,51 @@
 #!/bin/bash
 
-# update system
-sudo apt update
-sudo apt upgrade
+if hash apt 2>/dev/null; then
+    # update system
+    sudo apt update
+    sudo apt upgrade
+    
+    # install haskell
+    sudo apt install haskell-platform -yqq
+    
+    # install chromium browser
+    sudo apt install chromium-browser -yqq
+    
+    # install verion controll (svn and git)
+    sudo apt install subversion git -yqq
+    
+    # install curl
+    sudo apt install curl -yqq
 
-# install haskell
-sudo apt install haskell-platform -yqq
+    # install vscode
+    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+    sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+    sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+    
+    sudo apt install apt-transport-https
+    sudo apt update
+    sudo apt install code -yqq # or code-insiders
 
-# install chromium browser
-sudo apt install chromium-browser -yqq
+else
+    # update system
+    sudo pacman -Syyu
+    
+    # install haskell
+    sudo pacman -S stack
+    
+    # install chromium browser
+    sudo pacman -S chromium
+    
+    # install verion controll (svn and git)
+    sudo pacman -S subversion git
+    
+    # install curl
+    sudo pacman -S curl
 
-# install verion controll (svn and git)
-sudo apt install subversion git -yqq
-
-# install curl
-sudo apt install curl -yqq
-
-# install vscode
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-
-sudo apt install apt-transport-https
-sudo apt update
-sudo apt install code -yqq # or code-insiders
+    #install vscode
+    sudo pacman -S code
+fi
 
 # set vscode as the default editor (https://wiki.debian.org/DebianAlternative)
 sudo update-alternatives --set editor /usr/bin/code
+
