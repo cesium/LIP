0da8645490a544f5227c2acf18a2d892c149cc40
Pedro Mendes <pedro.mendes.26@gmail.com>
2019-09-28 12:13:37 +0100

    Remove need to confirm arch installations (#9)

--- install.sh
+++ install.sh
@@ -4,16 +4,16 @@ if hash apt 2>/dev/null; then
     # update system
     sudo apt update
     sudo apt upgrade
-    
+
     # install haskell
     sudo apt install haskell-platform -yqq
-    
+
     # install chromium browser
     sudo apt install chromium-browser -yqq
-    
+
     # install verion controll (svn and git)
     sudo apt install subversion git -yqq
-    
+
     # install curl
     sudo apt install curl -yqq
 
@@ -21,7 +21,7 @@ if hash apt 2>/dev/null; then
     curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
     sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
     sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-    
+
     sudo apt install apt-transport-https
     sudo apt update
     sudo apt install code -yqq # or code-insiders
@@ -31,22 +31,22 @@ if hash apt 2>/dev/null; then
 
 else
     # update system
-    sudo pacman -Syyu
-    
+    yes | sudo pacman -Syyu
+
     # install haskell
-    sudo pacman -S stack
-    
+    yes | sudo pacman -S stack
+
     # install chromium browser
-    sudo pacman -S chromium
-    
+    yes | sudo pacman -S chromium
+
     # install verion controll (svn and git)
-    sudo pacman -S subversion git
-    
+    yes | sudo pacman -S subversion git
+
     # install curl
-    sudo pacman -S curl
+    yes | sudo pacman -S curl
 
     #install vscode
-    sudo pacman -S code
+    yes | sudo pacman -S code
 fi
 
 # install Haskell Syntax Highlighting extension
