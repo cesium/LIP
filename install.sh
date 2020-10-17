d8bb573e1c2565a6931ca21a73dc7b6addc294e1
mendess <pedro.mendes.26@gmail.com>
2020-10-17 19:30:27 +0100

    Call the package manager less times to improve pfomance

--- install.sh
+++ install.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
-if hash apt 2>/dev/null; then
+set -e
+if command -v apt 2>/dev/null; then
     # update system
     sudo apt update
     sudo apt upgrade
 
-    # install haskell
-    sudo apt install haskell-platform -yqq
+    pkgs=(
+        haskell-platform # install haskell
+        subversion git # install verion controll (svn and git)
+        curl # install curl
+    )
 
-    # install chromium browser
-    sudo apt install chromium-browser -yqq
-
-    # install verion controll (svn and git)
-    sudo apt install subversion git -yqq
-
-    # install curl
-    sudo apt install curl -yqq
+    sudo apt install -yqq "${pkgs[@]}"
 
     # install vscode
-    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+    curl https://packages.microsoft.com/keys/microsoft.asc |
+        gpg --dearmor > microsoft.gpg
     sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
-    sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+    echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" |
+        sudo tee /etc/apt/sources.list.d/vscode.list
 
     sudo apt install apt-transport-https
     sudo apt update
@@ -28,25 +27,16 @@ if hash apt 2>/dev/null; then
 
     # set vscode as the default editor (https://wiki.debian.org/DebianAlternative)
     sudo update-alternatives --set editor /usr/bin/code
-
 else
+    pkgs=(
+        stack # install haskell
+        firefox # install firefox
+        subversion git # install version controll
+        code # install vscode
+        discord # install discord
+    )
     # update system
-    yes | sudo pacman -Syyu
-
-    # install haskell
-    yes | sudo pacman -S stack
-
-    # install chromium browser
-    yes | sudo pacman -S chromium
-
-    # install verion controll (svn and git)
-    yes | sudo pacman -S subversion git
-
-    # install curl
-    yes | sudo pacman -S curl
-
-    #install vscode
-    yes | sudo pacman -S code
+    sudo pacman -Syyu --noconfirm --needed "${pkgs[@]}"
 fi
 
 # install Haskell Syntax Highlighting extension
